b9c66df84544201f51aa8219b7547f7e536bc704
Ramon Henrique <ramon.dsl@outlook.com>
2026-03-28 19:39:51 -0300

    finish Sucess skeleton component

--- src/pages/Sucess/index.tsx
+++ src/pages/Sucess/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import { Container } from './styles';
+import Title from '../../components/Title';
+import Paragraph from '../../components/Paragraph';
 
 const Sucess: React.FC = () => {
 	return (
 		<Container>
+			<img src="/images/icon-thank-you.svg" />
+
+			<Title>Thank you!</Title>
+			<Paragraph>
+				Thanks for confirming your subscription! We hope you have fun using
+				our platform. If you ever need support, please feel free to email us
+				at support@loremgaming.com.
+			</Paragraph>
 		</Container>
 	);
 };
