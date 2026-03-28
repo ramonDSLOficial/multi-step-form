9208cb8285d1d1a38b5b59cab5692e62643ebf31
Ramon Henrique <ramon.dsl@outlook.com>
2026-03-28 07:05:34 -0300

    FourthStep skeleton finish

--- src/pages/FourthStep/index.tsx
+++ src/pages/FourthStep/index.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+	ChangePlanbtn,
+	Clause,
+	Container,
+	PlanDetails,
+	Price,
+	Term,
+} from './styles';
+import Title from '../../components/Title';
+import Paragraph from '../../components/Paragraph';
+import { PATHS } from '../../routes/paths';
 
 const FourthStep: React.FC = () => {
 	return (
 		<Container>
+			<Title>finishing up</Title>
+			<Paragraph>
+				Double-check everything looks OK before confirming.
+			</Paragraph>
+
+			<section>
+				<PlanDetails>
+					<section>
+						<Clause>
+							<div>
+								<Term>Arcade (monthly)</Term>
+								<ChangePlanbtn to={PATHS.steps.second}>
+									change
+								</ChangePlanbtn>
+							</div>
+
+							<Price className="plan">$9/mo</Price>
+						</Clause>
+
+						<Clause>
+							<Term>online service</Term>
+							<Price>+$1/mo</Price>
+						</Clause>
+
+						<Clause>
+							<Term>larger storage</Term>
+							<Price>+$2/mo</Price>
+						</Clause>
+					</section>
+
+					<Clause>
+						<Term>Total (per month)</Term>
+						<Price className="total">+$12/mo</Price>
+					</Clause>
+				</PlanDetails>
+			</section>
 		</Container>
 	);
 };
--- src/pages/FourthStep/styles.ts
+++ src/pages/FourthStep/styles.ts
@@ -1,4 +1,14 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Container = styled.section``;
 
+export const PlanDetails = styled.section``;
+
+export const ChangePlanbtn = styled(Link)``;
+
+export const Clause = styled.div``;
+
+export const Term = styled.p``;
+
+export const Price = styled.p``;
