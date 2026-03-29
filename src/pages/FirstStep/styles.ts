0684437009af91ffbec14b734d921b4e0cd84dbc
Ramon Henrique <ramon.dsl@outlook.com>
2026-03-29 06:48:55 -0300

    FirstStep container styles finish

--- src/pages/FirstStep/styles.ts
+++ src/pages/FirstStep/styles.ts
@@ -1,4 +1,11 @@
 import styled from 'styled-components';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+    width: 90%;
+    padding: 25px 20px;
+    border-radius: 5px;
+    box-shadow: -1px 1px 10px #00000024;
+    margin-top: -32px;
+    background-color: ${props => props.theme.colors.neutral.fifth};
+`;
 
