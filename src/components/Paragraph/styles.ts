efa891c2f7a30bb85ffda86ad86bfbf689a66b44
Ramon Henrique <ramon.dsl@outlook.com>
2026-03-29 07:13:48 -0300

    finish Title and Paragraph styles

--- src/components/Paragraph/styles.ts
+++ src/components/Paragraph/styles.ts
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.p`
+    color: ${(props) => props.theme.colors.neutral.first};
+	font-size: 1.4rem;
+	font-weight: ${(props) => props.theme.font.weight.terciary};
 `;
--- src/components/Title/styles.ts
+++ src/components/Title/styles.ts
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.h1`
-	color: ${(props) => props.theme.colors.primary.first};
+	color: ${(props) => props.theme.colors.neutral.black};
 	font-size: 2.2rem;
 	font-weight: ${(props) => props.theme.font.weight.primary};
+	margin-bottom: 10px;
 `;
