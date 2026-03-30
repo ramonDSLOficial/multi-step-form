8302fadd5ea4c158b85cac56d39ce3186405c5e9
Ramon Henrique <ramon.dsl@outlook.com>
2026-03-30 17:16:03 -0300

    up: centralizando styles dos step no Form

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 
 export const Container = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    width: inherit;
-    height: inherit;
-    background-color: ${props => props.theme.colors.neutral.third};
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	width: inherit;
+	height: inherit;
+	background-color: ${(props) => props.theme.colors.neutral.third};
+
+	& > section:first-of-type {
+		width: 90%;
+		padding: 25px 20px;
+		border-radius: 5px;
+		box-shadow: -1px 1px 10px #00000024;
+		margin-top: -32px;
+		background-color: ${(props) => props.theme.colors.neutral.white};
+	}
 `;
--- src/pages/FirstStep/styles.ts
+++ src/pages/FirstStep/styles.ts
@@ -1,13 +1,6 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-    width: 90%;
-    padding: 25px 20px;
-    border-radius: 5px;
-    box-shadow: -1px 1px 10px #00000024;
-    margin-top: -32px;
-    background-color: ${props => props.theme.colors.neutral.white};
-
     & > section {
         display: flex;
         flex-direction: column;
