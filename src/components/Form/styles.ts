15fd2c24f5bc50b9c3143057737aa278162c1b2c
Ramon Henrique <ramon.dsl@outlook.com>
2026-04-10 17:34:29 -0300

    fix: Slidebar background-image.

--- src/components/Form/styles.ts
+++ src/components/Form/styles.ts
@@ -14,7 +14,7 @@ export const Container = styled.form`
 		padding: 25px 20px;
 		border-radius: 5px;
 		box-shadow: -1px 1px 10px #00000024;
-		margin-top: -32px;
+		margin-top: -40px;
 		background-color: ${(props) => props.theme.colors.neutral.white};
 	}
 `;
--- src/components/Slidebar/styles.ts
+++ src/components/Slidebar/styles.ts
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 export const Container = styled.section`
     display: flex;
     justify-content: center;
-    align-items: center;
+    /* align-items: center; */
+    padding-top: 25px;
     gap: 15px;
     width: 100%;
-    height: 20vh;
-    /* color img pos repe attachement */
-    /* size */
-    background: ${props => props.theme.colors.neutral.first} url("/public/images/bg-sidebar-mobile.svg") ;
+    height: 23vh;
+    max-height: 130px;
+    background: ${props => props.theme.colors.neutral.first} url("/images/bg-sidebar-mobile.svg") top no-repeat;
+    background-size: cover;
     aspect-ratio: 16 / 9;
-    object-fit: cover;
-`;
+`; 
