bfbb7d784bf0642aaca6c41cc5a0dab160311276
Ramon Henrique <ramon.dsl@outlook.com>
2026-04-11 00:18:41 -0300

    style: atualizando o background.

--- src/components/Slidebar/styles.ts
+++ src/components/Slidebar/styles.ts
@@ -3,13 +3,11 @@ import styled from 'styled-components';
 export const Container = styled.section`
     display: flex;
     justify-content: center;
-    /* align-items: center; */
     padding-top: 25px;
     gap: 15px;
-    width: 100%;
     height: 23vh;
     max-height: 130px;
     background: ${props => props.theme.colors.neutral.first} url("/images/bg-sidebar-mobile.svg") top no-repeat;
+    aspect-ratio: 24 / 9;
     background-size: cover;
-    aspect-ratio: 16 / 9;
 `; 
