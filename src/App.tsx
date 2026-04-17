af0486f2d002c30f965eae9cbb12cc46a9e45691
Ramon Henrique <ramon.dsl@outlook.com>
2026-04-17 09:26:54 -0300

    fix: corrigindo path de import.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container, Main } from './styles/appStyles';
 import Footer from './components/Footer';
-import Slidebar from './components/Slidebar';
+import Sidebar from './components/Sidebar';
 import Form from './components/Form';
 import FormFooter from './components/FormFooter';
 
@@ -11,7 +11,7 @@ const App: React.FC = () => {
 	return (
 		<Container>
 			<Main>
-				<Slidebar />
+				<Sidebar />
 				
 				<Form>
 					<Outlet />
