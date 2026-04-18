c2eb609da03a16fe68e88ef2b897896cd55b6797
Ramon Henrique <ramon.dsl@outlook.com>
2026-04-18 09:37:10 -0300

    style: Title e Paragraph ajuste par a desktop.

--- src/components/Paragraph/styles.ts
+++ src/components/Paragraph/styles.ts
@@ -4,4 +4,8 @@ export const Container = styled.p`
     color: ${(props) => props.theme.colors.neutral.first};
 	font-size: 1.4rem;
 	font-weight: ${(props) => props.theme.font.weight.terciary};
+
+	@media screen and (min-width: 769px) {
+		margin-bottom: 35px;
+	}
 `;
--- src/components/Title/styles.ts
+++ src/components/Title/styles.ts
@@ -5,4 +5,10 @@ export const Container = styled.h1`
 	font-size: 2.2rem;
 	font-weight: ${(props) => props.theme.font.weight.primary};
 	margin-bottom: 10px;
+	
+	@media screen and (min-width: 769px) {
+		font-size: 2.6rem;
+		color: ${(props) => props.theme.colors.primary.first};
+		text-shadow: 1px 1px 0px #022a5a45;
+	}
 `;
