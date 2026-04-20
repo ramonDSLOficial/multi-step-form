384391cda97a66e5fe1b46eb537571b56ca2d4b6
Ramon Henrique <ramon.dsl@outlook.com>
2026-04-20 01:09:34 -0300

    style: aumentando icon de Thank you de Sucess.

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -5,11 +5,7 @@ import { Container } from './styles';
 // vai virar o formprivder
 // mudar para form atributes
 const Form: React.FC<FormHTMLAttributes<HTMLFormElement>> = ({ children }) => {
-  return (
-    <Container>      
-        { children }
-    </Container>
-  );
-}
+	return <Container>{children}</Container>;
+};
 
 export default Form;
--- src/pages/Sucess/styles.ts
+++ src/pages/Sucess/styles.ts
@@ -1,16 +1,25 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-    text-align: center;
-    line-height: 2rem;
-    
-    form > && {
-        padding: 90px 20px;
-    }
+	text-align: center;
+	line-height: 2rem;
 
-    & > img {
-        width: 20%;
-        max-width: 45px;
-        margin-bottom: 15px;
-    }
+	form > && {
+		padding: 90px 20px;
+	}
+
+	& > img {
+		width: 18%;
+		max-width: 45px;
+		margin-bottom: 15px;
+	}
+
+	@media screen and (min-width: 769px) {
+		form > && {
+			padding: 90px 0px;
+		}
+		& > img {
+			max-width: 64.75px;
+		}
+	}
 `;
