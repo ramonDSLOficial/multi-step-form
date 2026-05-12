ed7d16c6c13ae1bd1786c70ec62d47717eb86ba5
Ramon Henrique <ramon.dsl@outlook.com>
2026-05-12 00:21:02 -0300

    refactor: alterando forma de construção dos inputs radio.

--- src/pages/SecondStep/index.tsx
+++ src/pages/SecondStep/index.tsx
@@ -15,84 +15,63 @@ const SecondStep: React.FC = () => {
 
 			<FormContanier>
 				<section>
-					<Input.Root $type="radio" inputClassName="plan">
-						<Input.Icon
-							source="/images/icon-arcade.svg"
-							alt="arcade plan icon"
-						/>
+					{['arcade', 'advanced', 'pro'].map((planType) => {
+						return (
+							<Input.Root $type="radio" inputClassName="plan">
+								<Input.Icon
+									source={`/images/icon-${planType}.svg`}
+									alt={`${planType} plan icon`}
+								/>
 
-						<div>
-							<Input.Label label="arcade" />
-							<Input.Description>$9/mo</Input.Description>
-							<Input.Description className="advantage">
-								2 months free
-							</Input.Description>
-							<Input.InputElement name="plan" />
-						</div>
-					</Input.Root>
-
-					<Input.Root $type="radio" inputClassName="plan">
-						<Input.Icon
-							source="/images/icon-advanced.svg"
-							alt="advanced plan icon "
-						/>
-
-						<div>
-							<Input.Label label="advanced" />
-							<Input.Description>$12/mo</Input.Description>
-							<Input.Description className="advantage">
-								2 months free
-							</Input.Description>
-							<Input.InputElement name="plan" />
-						</div>
-					</Input.Root>
-
-					<Input.Root $type="radio" inputClassName="plan">
-						<Input.Icon
-							source="/images/icon-pro.svg"
-							alt="pro plan icon"
-						/>
-
-						<div>
-							<Input.Label label="pro" />
-							<Input.Description>$15/mo</Input.Description>
-							<Input.Description className="advantage">
-								2 months free
-							</Input.Description>
-							<Input.InputElement name="plan" />
-						</div>
-					</Input.Root>
+								<div>
+									<Input.Label label={planType} />
+									<Input.Description>$9/mo</Input.Description>
+									<Input.Description className="advantage">
+										2 months free
+									</Input.Description>
+									<Input.InputElement />
+								</div>
+							</Input.Root>
+						);
+					})}
 				</section>
 
 				<section>
-					{<Controller
-						name="modality"
-						control={control}
-						render={({ field }) => {
-							return (
-								<Input.Root $type="checkbox" inputClassName="toggle">
-									<Input.Label
-										label="monthly"
-										className={!field.value ? 'selected' : ''}
-									/>
+					{
+						<Controller
+							name="modality"
+							control={control}
+							render={({ field }) => {
+								return (
+									<Input.Root
+										$type="checkbox"
+										inputClassName="toggle"
+									>
+										<Input.Label
+											label="monthly"
+											className={
+												!field.value ? 'selected' : ''
+											}
+										/>
 
-									<Input.InputElement
-										{...field}
-										value={undefined}
-										checked={field.value}
-										onChange={(e) =>
-											field.onChange(e.target.checked)
-										}
-									/>
+										<Input.InputElement
+											{...field}
+											value={undefined}
+											checked={field.value}
+											onChange={(e) =>
+												field.onChange(e.target.checked)
+											}
+										/>
 
-									<Input.Label
-										label="yearly"
-										className={field.value ? 'selected' : ''}
-									/>
-								</Input.Root>
-							);
-						}}
-					/>}
+										<Input.Label
+											label="yearly"
+											className={field.value ? 'selected' : ''}
+										/>
+									</Input.Root>
+								);
+							}}
+						/>
+					}
 				</section>
 			</FormContanier>
 		</Container>
