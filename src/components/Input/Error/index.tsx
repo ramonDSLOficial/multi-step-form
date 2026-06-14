0a2b02fc9dcd238b857f8bb19831e4ea544380c0
Ramon Henrique <ramon.dsl@outlook.com>
2026-06-14 09:01:13 -0300

    chore: deixando chegar as props aria para o Error.

--- src/components/Input/Error/index.tsx
+++ src/components/Input/Error/index.tsx
@@ -3,11 +3,18 @@ import { useInputContext } from '../InputContext';
 
 import { ErrorElement } from './styles';
 
-const Error: React.FC<HTMLAttributes<HTMLSpanElement>> = ({ className }) => {
+const Error: React.FC<HTMLAttributes<HTMLSpanElement>> = ({
+	className,
+	...props
+}) => {
 	const { $type, helperText, inputClassName } = useInputContext();
 
 	return (
-		<ErrorElement $type={$type} className={`${inputClassName} ${className}`}>
+		<ErrorElement
+			$type={$type}
+			className={`${inputClassName} ${className}`}
+			{...props}
+		>
 			{helperText}
 		</ErrorElement>
 	);
