62cd60e8d2fd21d234303945cd363318ba8575ce
Ramon Henrique <ramon.dsl@outlook.com>
2026-08-14 11:23:10 -0300

    feat: adicionando link do github na âncora em Footer.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
   return (
     <Container>
       <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.</p>
-      <p>Coded by <a href="#">Ramon Henrique</a></p>.
+      <p>Coded by <a href="https://github.com/RamonDSL">Ramon Henrique</a></p>.
     </Container>
   );
 }
